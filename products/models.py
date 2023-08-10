fa3caa147779d3db24e94468491ea539e63f830c
domenicasantana <dasc1996@hotmail.com>
2023-08-10 17:42:19 +0000

    added products app

--- products/models.py
+++ products/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.Charfield(max_length=254)
-    friendly_name = models.Charfield(max_length=254, null=true, blank=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
     def get_friendly_name(self):
@@ -14,11 +14,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.Charfield(max_length=254)
+    name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=true, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
