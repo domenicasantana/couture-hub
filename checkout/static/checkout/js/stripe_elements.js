4292fc9aad42634d8e732a1e6e28a0e6c7594be2
domenicasantana <dasc1996@hotmail.com>
2023-09-10 19:31:32 +0000

    Fix settings variables keys

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -32,11 +32,11 @@ card.mount('#card-element');
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
-        var html =
+        var html =`
             <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
             </span><span>${event.error.message}</span>
-        ;
+        `;
         $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
